c6ffaa2c8ba17e54c1a0e332ccb8f137d469b049
shawki98 <shawkiize@gmail.com>
2023-01-18 16:33:01 -0500

    -

---
+++ scripts/render-menu-preview.js
@@ -0,0 +1,22 @@
+// fetch the watch data from our server
+const response = await fetch("/Menu.json");
+let Menu = await response.json();
+Menu = Menu.slice(0, 3);
+
+// get the product-cards-grid element
+const grid = document.querySelector('.productCardGrid');
+
+// get the product-card template, remove it from the grid.
+const cardTemplate = document.querySelector(".productCard");
+grid.removeChild(cardTemplate);
+
+for (let i = 0; i < Menu.length; i++) {
+    // clone the template
+    const card = cardTemplate.cloneNode(true);
+    // set the data
+    card.querySelector(".productCard-Foodtype").textContent = Menu[i].Foodtype;
+    card.querySelector(".productCard-details").textContent = Menu[i].details;
+    card.querySelector(".productCard-image").src = Menu[i].image;
+    // add the card to the grid
+    grid.appendChild(card);
+}
---
+++ scripts/render-menu.js
@@ -0,0 +1,26 @@
+// fetch the watch data from our server
+const response = await fetch("/Menu.json");
+const Menu = await response.json();
+
+console.log(Menu); 
+console.log(response);
+console.log("hello world");
+
+// get the product-cards-grid element
+const grid = document.querySelector('.productCardGrid');
+
+// get the product-card template, remove it from the grid.
+const cardTemplate = document.querySelector(".productCard");
+grid.removeChild(cardTemplate);
+
+// loop through the data and create a card for each item
+for  (let i = 0; i < Menu.length; i++) {
+    // clone the template
+    const card = cardTemplate.cloneNode(true);
+    // set the data
+    card.querySelector(".productCard-Foodtype").textContent = Menu[i].Foodtype;
+    card.querySelector(".productCard-details").textContent = Menu[i].details;
+    card.querySelector(".productCard-image").src = Menu[i].image;
+    // add the card to the grid
+    grid.appendChild(card);
+}
